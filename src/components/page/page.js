e9aed4b6673616d91b87b6cff5cb92dfdadb2403
Ivan Bilan <bilan.vanya20044@gmail.com>
2023-01-11 09:46:24 +0000

    added banner and services

--- src/components/page/page.js
+++ src/components/page/page.js
@@ -19,6 +19,31 @@ const Page = () => {
                     </ul>
                 </nav>
             </header>
+            <div className="banner">
+                <h1>car looking <span>scruffy?</span></h1>
+                <h2>Time for a scrub!</h2>
+                <h3>Need a good detailing? We do that too.</h3>
+                <button className="services-btn">View Our Services</button>
+            </div>
+            <h2 className="selection">Select Your Wash</h2>
+            <div className="buttons-section">
+                <button className="select-button">Small Suds</button>
+                <button className="select-button">Premium Suds</button>
+                <button className="select-button">Members Only</button>
+                <button className="select-button">Extreme Suds</button>
+            </div>
+            <div className="services">
+                <div className="interior-service design">
+                    <div className="service-header">
+                        Interior
+                    </div>
+                </div>
+                <div className="exterior-service design">
+                    <div className="service-header">
+                        exterior
+                    </div>
+                </div>
+            </div>
           </div>
         </>
     )
